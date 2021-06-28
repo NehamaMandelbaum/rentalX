9bc292f16dcd830fda3fd010183624acb0ede240
NehamaMandelbaum <nehama1836@gmail.com>
2021-06-28 11:30:23 -0300

    categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,33 @@
 import {Router} from 'express'
 
-import {Category} from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router(); 
+const categoriesRepository = new CategoriesRepository() 
 
 
 
 categoriesRoutes.post('/', (request, response) => {
     const {name, description} = request.body; 
 
+    const categoryAlreadyExists = categoriesRepository.findByName(name)
 
-    return response.status(201).json({ category })
+    if (categoryAlreadyExists) {
+        return response.status(400).json({error: "Category Already Exists!"})
+    }
+
+    categoriesRepository.create({name, description})
+
+
+    return response.status(201).send()
 })
 
+categoriesRoutes.get('/', (request, response) => {
+
+   const all = categoriesRepository.list()
+
+   response.json(all)
+})
 
 
 export {categoriesRoutes} 
